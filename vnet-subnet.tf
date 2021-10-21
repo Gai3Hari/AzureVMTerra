668cada5b0bb04ed86550d6e32579673c4cf54bb
Gai3Hari <gai3devi@gmail.com>
2021-10-20 23:01:40 -0400

    Create vnet-subnet.tf

---
+++ vnet-subnet.tf
@@ -0,0 +1,14 @@
+resource "azurerm_virtual_network" "myterraformnetwork" {
+  name = var.vnetname
+  address_space = ["10.0.0.0.16"]
+  location = var.location
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name  
+  tags = var.tags
+  }
+resource "azurerm_subnet" "myterraformsubnet" {
+  name = var.subnetname
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name 
+  virtual_network_name = azurerm_virtual_network.myTerraformnetwork.name
+  address_prefixes = ["10.0.2.0/24"]
+  tags = var.tags
+  }
