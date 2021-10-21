4d062d30f467dbff0a736dec0360f28df3fdabe1
Gai3Hari <gai3devi@gmail.com>
2021-10-20 23:22:56 -0400

    Update vnet-subnet.tf

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -10,5 +10,5 @@ resource "azurerm_subnet" "myterraformsubnet" {
   resource_group_name = azurerm_resource_group.myTerraformGroup.name 
   virtual_network_name = azurerm_virtual_network.myTerraformnetwork.name
   address_prefixes = ["10.0.2.0/24"]
-  tags = var.tags
+ 
   }
