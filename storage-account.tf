19e8b46982af149a01fef23ebe4cddd373c83e7d
Gai3Hari <gai3devi@gmail.com>
2021-10-25 23:03:50 -0400

    Update storage-account.tf
    updated file with storage account details

--- storage-account.tf
+++ storage-account.tf
@@ -1,11 +1,16 @@
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
+resource "random-id" "randomId" {
+  keepers = {
+    resource_group = azurerm_resource_group.myTerraformGroup.name
   }
+  byte_length = 8
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "diag${random_id.randomId.hex}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS "
+
+  tags = var.tags
 }
