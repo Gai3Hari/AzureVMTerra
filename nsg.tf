9c666954a10145d1b33945fe26f985425c23109d
Gai3Hari <gai3devi@gmail.com>
2021-10-25 22:06:40 -0400

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -1,21 +1,19 @@
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_security_group" "myterraformnsg" {
+  name                = var.nsgname
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name 
 
   security_rule {
-    name                       = "test123"
+    name                       = "SSH"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
-  tags = {
-    environment = "Production"
-  }
+  tags = var.tags
 }
