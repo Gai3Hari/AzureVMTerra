057d68fab262c7adbae51a3889882b59ea75a679
Gai3Hari <gai3devi@gmail.com>
2021-10-25 22:37:53 -0400

    Update nic.tf
    Updated variables in the script

--- nic.tf
+++ nic.tf
@@ -1,11 +1,13 @@
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "myTerraformNic" {
+  name                = var.nicname
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.myterraformsubnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.example.id
   }
+  tags = var.tags
 }
