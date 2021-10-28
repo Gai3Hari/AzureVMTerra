3dc7f3b86d4aa96731040cc006db9776a5dd66f8
Gai3Hari <gai3devi@gmail.com>
2021-10-27 23:38:39 -0400

    Update virtual-machine.tf

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -11,7 +11,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = tls_private_key.ssh-key..ssh-key.public_key_openssh
   }
    os_disk {
     caching              = "ReadWrite"
